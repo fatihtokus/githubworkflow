10c6062bcf8fd08807058e328b3fa926fb3d4cc1
Fatih Tokus <fatih.tokus@gmail.com>
2022-12-28 22:46:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                      = "us-east-1"
-  skip_credentials_validation = true
+  skip_credentials_validation = false
   skip_requesting_account_id  = true
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
